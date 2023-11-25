256effb0e42889e075c6bf302f39053961807e90
SeungminYun-G-B <147472555+SeungminYun-G-B@users.noreply.github.com>
2023-11-25 21:27:12 +0900

    세 번째 시도 돌입
    어트랙터와 레펠러 없이 파티클끼리 서로 이끌리게 해보고자 했습니다

--- sketch.js
+++ sketch.js
@@ -1,13 +1,11 @@
 
 let emitter;
-let emitter2;
 let repeller;
 let att;
 
 function setup() {
   createCanvas(800, 1000);
   emitter = new Emitter(width / 2, 0);
-  emitter2 = new Emitter(width/2,height);
   repeller = new Repeller(width / 2, 150);
   att = new Attractor(width/2, height-150);
 }
@@ -15,33 +13,16 @@ function setup() {
 function draw() {
   background(255);
 
-
-  repeller.move(1);
-  att.move(1);
-
-
   for(let i=0; i<10; i++){
    emitter.addParticle();
   }
-  for(let i=0; i<10; i++){
-    emitter2.addParticle();
-   }
-      
 
   let gravity = createVector(0, 0.1);
   emitter.applyForce(gravity);
 
-  let gravity2 = createVector(0,-0.1);
-   emitter2.applyForce(gravity2);
 
-  emitter.applyRepeller(repeller);
-  emitter.applyAttractor(att);
   emitter.run();
-  emitter2.applyRepeller(repeller);
-  emitter2.applyAttractor(att);
-  emitter2.run();
+
  
 
-  repeller.show();
-  att.show();
 }
