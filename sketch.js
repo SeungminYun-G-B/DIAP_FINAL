17ce67a22fa13962c02503c909523467765f7b66
SeungminYun-G-B <147472555+SeungminYun-G-B@users.noreply.github.com>
2023-11-26 03:29:58 +0900

    어트랙터,리펠러 추가
    어트랙터와 리펠러를 각각 복사본을 만들고 각 복사본은 코사인, 탄젠트 값에 변형을 주어서 대칭이 이뤄지게 만들었습니다. (이번 과정은 p5.js에서 미리 만들어보고 옮겼습니다.)

--- sketch.js
+++ sketch.js
@@ -1,32 +1,40 @@
 let emitter;
 let repeller;
+let repeller2;
 let att;
+let att2;
 
 function setup() {
   createCanvas(800, 800);
   emitter = new Emitter(width / 2, height/2);
-  repeller = new Repeller(width / 2, 150);
-  att = new Attractor(width/2, height-150);
+  repeller = new Repeller(width / 2, height/2);
+  repeller2 = new Repeller2(width / 2, height/2);
+  att = new Attractor(width/2, height/2);
+  att2 = new Attractor2(width/2, height/2);
 }
 
 function draw() {
   background(255);
+  repeller.move();
+  repeller2.move();
+  att.move();
+  att2.move();
 
 
-  repeller.move(1);
-  att.move(1);
-
-
-  for(let i=0; i<10; i++){
+  for(let i=0; i<15; i++){
    emitter.addParticle();
   }
       
 
   emitter.applyRepeller(repeller);
+  emitter.applyRepeller(repeller2);
   emitter.applyAttractor(att);
+  emitter.applyAttractor(att2);
   emitter.run();
  
 
   repeller.show();
+  repeller2.show();
   att.show();
+  att2.show();
 }
