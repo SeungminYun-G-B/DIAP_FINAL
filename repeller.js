8365b680a90caaeb78db1a3f3e744c344dd3ee79
SeungminYun-G-B <147472555+SeungminYun-G-B@users.noreply.github.com>
2023-11-26 16:39:38 +0900

    리펠러 복사
    리펠러를 두 개로 만들고 두 개가 너무 대칭으로 움직이지 않게 값을 조금 조정해주었습니다

--- repeller.js
+++ repeller.js
@@ -1,15 +1,15 @@
-class Repeller {
+class Repeller2 {
   constructor(x, y) {
     this.position = createVector(x, y);
     this.speed = 200;
     this.val = 1;
-    this.power = 500;
+    this.power = 1500;
   }
   
   move() {
     this.t =frameCount;
-    this.position.x = this.speed * sin(this.t * 0.07) + width/2;
-    this.position.y = this.speed * cos(this.t * 0.07) + height/2;
+    this.position.x = this.speed * sin(this.t * 0.07)+ width/2;
+    this.position.y = this.speed * cos(this.t * 0.07)+ height/2;
     this.speed += this.val
     if(this.speed>350 || this.speed <100){
       this.val*= -1;
--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 let emitter;
 let emitter2;
 let repeller;
+let repeller2;
 let att;
 
 function setup() {
@@ -8,12 +9,14 @@ function setup() {
   emitter = new Emitter(width / 2, height/2-70);
   emitter2 = new Emitter(width/2, height/2+70);
   repeller = new Repeller(width / 2, height/2);
+  repeller2 = new Repeller2(width/2, height/2)
   att = new Attractor(width/2, height/2);
 }
 
 function draw() {
   background(255);
   repeller.move();
+  repeller2.move();
   att.move();
 
 
@@ -28,13 +31,16 @@ function draw() {
       
 
   emitter.applyRepeller(repeller);
+  emitter.applyRepeller(repeller2);
   emitter.applyAttractor(att);
   emitter.run();
   emitter2.applyRepeller(repeller);
+  emitter2.applyRepeller(repeller2);
   emitter2.applyAttractor(att);
   emitter2.run();
  
 
   repeller.show();
+  repeller2.show();
   att.show();
 }
